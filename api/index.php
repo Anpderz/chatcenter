24fa0677fad81cb4e4585afc697f346335324e7d
Anpderz <anpderz@gmail.com>
2026-05-12 23:10:09 -0500

    first commit

---
+++ api/index.php
@@ -0,0 +1,38 @@
+<?php
+
+/*=============================================
+Depurar Errores
+=============================================*/
+
+define('DIR',__DIR__);
+
+ini_set("display_errors", 1);
+ini_set("log_errors", 1);
+ini_set("error_log", DIR."/php_error_log");
+
+/*=============================================
+Variables de entorno
+=============================================*/
+
+require_once "vendor/autoload.php";
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__.'/../');
+$dotenv->load();
+
+/*=============================================
+CORS
+=============================================*/
+
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
+header('content-type: application/json; charset=utf-8');
+
+/*=============================================
+Requerimientos
+=============================================*/
+
+require_once "controllers/routes.controller.php";
+
+$index = new RoutesController();
+$index -> index();
